8b671462d0bbccd209830f2ed94ab5ac5c1856ea
gicadin <hristea52@gmail.com>
2015-04-21 15:19:05 -0700

    First commit for thompsoncreek

---
+++ httpdocs/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-04-15 16:07:07 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-04-15T16:07:07+00:00	INFO 69.196.67.100	joomlafailure	Username and password do not match or you do not have an account yet.
